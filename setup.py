633e41c8f6db66c62975caae847d8c6d2a1a1f0c
Chet Luther <cluther@zenoss.com>
2011-04-27 14:49:11 -0500

    Bumping version to 1.0.1.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.CloudFoundry"
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 AUTHOR = "Zenoss"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
