db66662edc2c3cb7f5ca0cdda8f5b50a30a4007b
Chet Luther <cluther@zenoss.com>
2011-04-27 09:19:04 -0500

    Bump version to 1.0.0.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.CloudFoundry"
-VERSION = "0.9.0"
+VERSION = "1.0.0"
 AUTHOR = "Zenoss"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
 PACKAGES = ['ZenPacks', 'ZenPacks.zenoss', 'ZenPacks.zenoss.CloudFoundry']
 INSTALL_REQUIRES = []
-COMPAT_ZENOSS_VERS = ">=3.0.3"
+COMPAT_ZENOSS_VERS = ">=3.1.0"
 PREV_ZENPACK_NAME = ""
 # STOP_REPLACEMENTS
 ################################
